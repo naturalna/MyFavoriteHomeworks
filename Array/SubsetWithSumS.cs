4dd7b41a5a8d6991c2825d79c3b22b3f840484a1
Anna Stoqnowa <naturalna89@gmail.com>
2013-02-17 17:55:57 +0200

    same projects

---
+++ Array/SubsetWithSumS.cs
@@ -0,0 +1,53 @@
+﻿//We are given an array of integers and a number S.
+//Write a program to find if there exists a subset of the elements of the array that has a sum S. 
+using System;
+
+
+class SubsetWithSumS
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int CheckSum = int.Parse(Console.ReadLine());
+        int[] arr = new int[n];
+        int mask;
+        int sum;
+        int count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+        for (int c = 0; c < (Math.Pow(2, n) - 1); c++)
+        {
+            mask = 1;
+            sum = 0;
+            count = 0;
+            do
+            {
+                if ((mask & c) == mask)
+                {
+                    sum += arr[n - 1 - count];
+                }
+                mask <<= 1;
+                count++;
+            } while (mask <= c);
+            if (CheckSum == sum)
+            {
+                Console.WriteLine("Yes");
+                mask = 1;
+                count = 0;
+                do
+                {
+                    if ((mask & c) == mask)
+                    {
+                        Console.WriteLine(arr[n - 1 - count]);
+                    }
+                    mask <<= 1;
+                    count++;
+                } while (mask <= c);
+                break;
+            }
+        }
+    }
+}
+
---
+++ Array2/KElementsWithSubsetS.cs
@@ -0,0 +1,54 @@
+﻿//Write a program that reads three integer numbers N, K and S and an array of N elements from the console. 
+//Find in the array a subset of K elements that have sum S or indicate about its absence.
+using System;
+
+class KElementsWithSubsetS
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int k = int.Parse(Console.ReadLine());
+        int s = int.Parse(Console.ReadLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+        for (int j = 1; j < Math.Pow(2,n); j++)
+        {
+            int mask = 1;
+            int counter = 0;
+            int counterSum = 0;
+            int sum = 0;
+            do
+            {
+                if ((mask & j) == mask)
+                {
+                    sum += arr[counterSum];
+                    counter++;
+                }
+                mask <<= 1;
+                counterSum++;
+            } while (mask <= j);
+            if (counter == k && sum == s)
+            {
+                Console.WriteLine("yes");
+                //for output 
+                mask = 1;
+                counterSum = 0;
+                while (mask < j)
+                {
+                    if ((mask & j) == mask)
+                    {
+                        Console.WriteLine("{0}",arr[counterSum]);
+                    }
+                    mask <<= 1;
+                    counterSum++;
+                }
+                break;
+            }
+        }
+    }
+}
+
